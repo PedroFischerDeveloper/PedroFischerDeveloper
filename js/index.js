a2c5b08defa7fe3137007f042ddd5310b75b2699
pedro fischer <pedro.fischer22@gmail.com>
2020-05-24 15:29:17 -0300

    [add] info warning, section projects

--- js/index.js
+++ js/index.js
@@ -1,3 +1,10 @@
+function onready() {
+    Swal.fire(
+        'Olá tudo bem, agradeço a visita',
+        'Antes de tudo gostaria de informar que o portfólio está construção, então agradeço a compreensão',
+        'info'
+    )
+}
 function openMenu() {
     var menuElement = document.querySelector('.header > nav > .menu');
 
